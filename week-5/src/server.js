29199e4b006d39c63c1b4ff3332696384c6cec15
Chankya000 <sgrsharma720@gmail.com>
2022-03-21 20:08:50 -0400

    week 10 activity

---
+++ week-5/src/server.js
@@ -0,0 +1,42 @@
+const http = require("http");
+
+const dataset = require("./controller/data");
+
+const parseURLParams = (value) => {
+  const params = new URLSearchParams(value);
+
+  return Array.from(params.entries()).reduce(
+    (acc, [key, value]) => ({ ...acc, [key]: value }),
+    {}
+  );
+};
+
+const server = http.createServer(async (req, res) => {
+  const [basePath, paramsString] = req.url.split("?");
+
+  if (basePath === "/api/datasets" && req.method === "GET") {
+    const params = parseURLParams(paramsString);
+
+    const { data, code } = await dataset.getAll(params);
+
+    res.writeHead(code, { "Content-Type": "application/json" });
+    res.end(data);
+
+  } else if (basePath.match(/\/api\/datasets\/\w+/) && req.method === "GET") {
+    const id = basePath.split("/")[3];
+
+    const { data, code } = await dataset.getById(id);
+
+    res.writeHead(code, { "Content-Type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Route Not Found" }));
+  }
+});
+
+const PORT = 8888;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = server;
